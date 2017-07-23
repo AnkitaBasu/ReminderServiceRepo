961d15f61856b18cddb7f8d49d26b82254293b61
AnkitaBasu <basu.moni2004@gmail.com>
2017-07-23 16:52:41 -0500

    Uploaded Crud and Service Module

---
+++ reminder-crud/src/main/java/com/exercise/reminder/orm/OrmReminderApplication.java
@@ -0,0 +1,22 @@
+package com.exercise.reminder.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class OrmReminderApplication
+ * @author Ankita Basu.
+ */
+@SpringBootApplication
+public class OrmReminderApplication {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(OrmReminderApplication.class, args);
+	}
+}
---
+++ reminder-service/src/main/java/com/exercise/reminder/exception/ExceptionTypes.java
@@ -0,0 +1,18 @@
+package com.exercise.reminder.exception;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Enum ExceptionTypes.
+ * @author Ankita Basu
+ */
+public enum ExceptionTypes {
+
+	/** The invalid. */
+	INVALID,
+	
+	/** The fatal. */
+	FATAL,
+	
+	/** The error. */
+	ERROR
+}
